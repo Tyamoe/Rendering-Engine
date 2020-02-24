31033945f13ec2abdc4665a2d9b087390c954ba2
Chris Thomas <chris@tyamoe.com>
2020-02-23 21:37:13 -0800

    .

--- Tracer/Utils.h
+++ Tracer/Utils.h
@@ -1,3 +1,23 @@
 #pragma once
 
-#include "Types.h"
+#include <random>
+
+template<typename T>
+T GetRand(T low, T high)
+{
+	if (low > high)
+	{
+		T temp = low;
+		low = high;
+		high = temp;
+	}
+
+	// fastest, but less accurate
+	//return ((float(rand()) / float(RAND_MAX)) * (high - low)) + low;
+
+	// super fast and super random
+	static std::random_device rd;
+	static std::mt19937 rng(rd());
+	std::uniform_real_distribution<> dist(low, high);
+	return (T)dist(rng);
+}
