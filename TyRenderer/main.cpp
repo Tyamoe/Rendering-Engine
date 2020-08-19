efda4ee07cb7e61e0280729c4fe7db86374642fa
Chris Thomas <chris@tyamoe.com>
2020-08-18 20:01:45 -0700

    ~Tyamoe3D integration

--- TyRenderer/main.cpp
+++ TyRenderer/main.cpp
@@ -8,15 +8,26 @@
 #include "Structs.h"
 #include "GenericDraw.h"
 
+#ifdef HEADLESS
+
 int main()
 {
 	Window window("Tray Racer", Settings(), Layout(800, 600, 400, 220));
 
 	GenericDraw::Init();
 
-	EnginePtr engine = new Engine({ RayTrace, Editor });
+	EnginePtr engine = new Engine({ Editor });
 	InputPtr input = new Input(window.GetGLFWWindow());
 
 	window.AttachInput(input);
 	window.AttachEngine(engine);
 }
+
+#else
+
+void Init()
+{
+	TYlog << "Hey\n";
+}
+
+#endif
